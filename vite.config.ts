429cc3ecf24f7a82dd1a07789405659b6f1ff7c7
maxim <trifonov.max@mail.ru>
2025-04-13 17:16:04 +0300

    removed svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
+import {defineConfig} from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react()],
   server: {open: true},
 });
